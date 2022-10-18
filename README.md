8ea91521cf591c73754055b6767c1c36846efb13
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-18 22:19:25 +0200

    Installing webpack.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fylo-landing-page
+
 A landing page for fictional company "Fylo". Frontend Mentor Challenge.
+Created by lilKriT.
+Using WebPack 5 and TailwindCSS.
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: { bundle: path.resolve(__dirname, "src/index.js") },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+      port: 3000,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+    },
+    module: {},
+    plugins: [],
+  },
+};
