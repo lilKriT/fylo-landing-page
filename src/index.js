85d49c04eb89bbdd577ec62cc23519dcbdf34892
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-19 20:20:26 +0200

    Configuring.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles/main.css";
+import logo from "./images/logo.svg";
+
+// console.log("lol");
--- webpack.config.js
+++ webpack.config.js
@@ -13,16 +13,21 @@ module.exports = {
   devtool: "source-map",
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, "dist/"),
     },
     port: 3000,
     open: true,
     hot: true,
     compress: true,
     historyApiFallback: true,
+    watchFiles: ["./public/*"],
   },
   module: {
     rules: [
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
       {
         test: /\.css$/i,
         include: path.resolve(__dirname, "src"),
