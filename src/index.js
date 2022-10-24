a120ae05471149fa73eac62c8e6337fc1c3319c4
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-24 22:55:15 +0200

    Working on validation.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./styles/main.css";
-import logo from "./images/logo.svg";
+// import logo from "./images/logo.svg";
 import "./template.html";
+
+const heroForm = document.querySelectorAll("#heroForm");
+const CTAForm = document.querySelectorAll("#CTAForm");
+
+const isValid = (s, regex) => {
+  return String(s).toLowerCase().match(regex);
+};
