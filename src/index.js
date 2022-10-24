f961b54ae4189417a16533a67d3ee80af11fbe41
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-24 23:11:08 +0200

    Validation.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import "./styles/main.css";
 // import logo from "./images/logo.svg";
 import "./template.html";
 
-const heroForm = document.querySelectorAll("#heroForm");
-const CTAForm = document.querySelectorAll("#CTAForm");
+const heroForm = document.querySelector("#heroForm");
+const CTAForm = document.querySelector("#CTAForm");
+
+const validate = (e) => {
+  e.preventDefault();
+  console.log("Validating");
+
+  if (true) {
+    e.target.submit();
+  }
+};
+
+heroForm.addEventListener("submit", (e) => validate(e));
 
 const isValid = (s, regex) => {
   return String(s).toLowerCase().match(regex);
