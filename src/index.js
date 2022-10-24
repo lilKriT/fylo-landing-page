f1375e29d646d681b3ca349db2751db3b0f3f2d1
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-24 23:37:15 +0200

    Validation.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import "./styles/main.css";
 // import logo from "./images/logo.svg";
 import "./template.html";
 
+// Taken from rfc 2822
+const emailRegex =
+  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
 const heroForm = document.querySelector("#heroForm");
 const CTAForm = document.querySelector("#CTAForm");
 
 const validate = (e) => {
   e.preventDefault();
-  console.log("Validating");
+  const currentEmail = e.target.elements["email"].value;
+  console.log("Validating: " + currentEmail);
 
-  if (true) {
-    e.target.submit();
+  if (isValid(currentEmail, emailRegex)) {
+    // e.target.submit();
+    console.log("Pass");
+  } else {
+    console.log("Fail");
   }
 };
 
